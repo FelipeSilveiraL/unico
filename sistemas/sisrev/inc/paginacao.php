34b2b3a846312834942d144b48c296d213c26339
FelipeLaras <felipesilveiralara@hotmail.com>
2022-08-11 12:30:17 -0300

    update

--- sistemas/sisrev/inc/paginacao.php
+++ sistemas/sisrev/inc/paginacao.php
@@ -1,42 +1,48 @@
 <?php
 session_start();
 
-
-require_once('../config/query.php');//query SISREV
+require_once('../config/query.php'); //query SISREV
 $tela = basename($_SERVER['PHP_SELF']);
-
-
 
 /* ################## CORES DO SISTEMAS  ################## */
 
 $querySistemaCores .=  ' WHERE id_usuario = ' . $_SESSION['id_usuario'] . ' AND id_sistema = ' . $_SESSION['id_sistema'];
 $resultado = $conn->query($querySistemaCores);
 
-if (!$coressistema = $resultado->fetch_assoc()) { $color = "#fff"; } else { $color = $coressistema['color']; }
+if (!$coressistema = $resultado->fetch_assoc()) {
+    $color = "#fff";
+} else {
+    $color = $coressistema['color'];
+}
 
-
-
-/* ################## PERMISSÕES  ################## */
-
-
-//SEGUNDO EU VEJO SE TENHO PERMISSÃO PARA ESTAR NESTA TELA!
-
-/* $queryModulosUser .= ' WHERE SUM.id_usuario = '.$_SESSION['id_usuario'];
-$resultadoModulosUser = $conn->query($queryModulosUser);
-
-while($modulosUser = $resultadoModulosUser->fetch_assoc()){
-    $endereco = explode('?', $modulosUser['endereco'], 2);
-
-    if($tela == $endereco[0]){
-        //echo 'é igual';
-    }else{
-        //echo 'é diferente';
-    }
-
-} */
 
 /* ################## PAGINAÇÃO  ################## */
 
 /* ==== PAGINAS ====*/
 //index.php
-if($tela == "index.php"){if($_GET['pg'] != NULL){echo '<script>window.location.href = "index.php";</script>';}}
+if ($tela == "index.php") {
+    if ($_GET['pg'] != NULL) {
+        echo '<script>window.location.href = "index.php";</script>';
+    }
+} else {
+    if ($_GET['pg'] == NULL) {
+        echo '<script>window.location.href = "index.php";</script>';
+        exit;
+    } else {
+
+        /* ################## PERMISSÕES  ################## */
+
+        $queryModulosUser2 = array('1' => ' WHERE SM.id = ' . $_GET['pg'] . ' AND SUM.id_usuario = ' . $_SESSION['id_usuario']);
+
+        $concatena = array_merge($queryModulosUser, $queryModulosUser2);
+        $queryModulosUserA = $concatena[0] . $concatena[1];
+
+
+        $resultadoModulosUser = $conn->query($queryModulosUserA);
+
+        if (!$modulosUser = $resultadoModulosUser->fetch_assoc()) {
+            echo '<script>window.location.href = "index.php";</script>';
+            exit;
+        }
+    }
+}
