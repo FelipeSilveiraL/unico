afbfffc3404cd8d903aa1c085f846ffa321333ae
FelipeLaras <felipesilveiralara@hotmail.com>
2022-08-11 07:57:59 -0300

    update

--- sistemas/sisrev/inc/print.php
+++ sistemas/sisrev/inc/print.php
@@ -2,7 +2,23 @@
 require_once('../config/query.php');
 
 // ------------------ ETIQUETA LASER -------------
+$dropTableEstoque = "DROP TABLE IF EXISTS sisrev_etiqueta_estoque ";
 
+$sucesso = $conn->query($dropTableEstoque);
+
+$createTableEst = "CREATE TABLE `sisrev_etiqueta_estoque`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `LOCACAO_ZONA` VARCHAR(80) NULL,
+    `LOCACAO_RUA` VARCHAR(80) NULL,
+    `LOCACAO_ESTANTE` VARCHAR(80) NULL,
+    `LOCACAO_PRATELEIRA` VARCHAR(80) NULL,
+    `ITEM_ESTOQUE` VARCHAR(80) NULL,
+    `LOCACAO_NUMERO` VARCHAR(80) NULL,
+    `EMPRESA` VARCHAR(80) NULL,
+    `REVENDA` VARCHAR(80) NULL,
+    PRIMARY KEY (`id`))";
+ 
+ $execCreate = $conn->query($createTableEst);
 
 $url = "http://10.100.1.215/unico_api/sisrev/api_estoque.php";
 $ch = curl_init($url);
@@ -11,19 +27,27 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 $resultado = json_decode(curl_exec($ch));
 
-foreach ($resultado->itensGaveta as $etiqLaser) {
+foreach ($resultado->itensEndereco as $etiqLaser) {
+    
   
     $insertEstoque = "INSERT INTO sisrev_etiqueta_estoque 
-                            (EMPRESA,LOCACAO_ESTANTE,ITEM_ESTOQUE,LOCACAO_NUMERO,LOCACAO_PRATELEIRA,REVENDA,LOCACAO_RUA,LOCACAO_ZONA)
+                            (LOCACAO_ZONA, 
+                            LOCACAO_RUA,
+                             LOCACAO_ESTANTE, 
+                             LOCACAO_PRATELEIRA, 
+                             ITEM_ESTOQUE, 
+                             LOCACAO_NUMERO, 
+                             EMPRESA, 
+                             REVENDA)
    
-    VALUES ('" . $etiqLaser->EMPRESA ."',
-            '" . $etiqLaser->LOCACAO_ESTANTE . "',
-            '" . $etiqLaser->ITEM_ESTOQUE . "' ,
-            '" . $etiqLaser->LOCACAO_NUMERO ."',
+    VALUES ('" . $etiqLaser->LOCACAO_ZONA ."',
+            '" . $etiqLaser->LOCACAO_RUA . "',
+            '" . $etiqLaser->LOCACAO_ESTANTE . "' ,
             '" . $etiqLaser->LOCACAO_PRATELEIRA ."',
-            '" . $etiqLaser->REVENDA ."',
-            '" . $etiqLaser->LOCACAO_RUA ."',
-            '" . $etiqLaser->LOCACAO_ZONA ."'
+            '" . $etiqLaser->ITEM_ESTOQUE ."',
+            '" . $etiqLaser->LOCACAO_NUMERO ."',
+            '" . $etiqLaser->EMPRESA ."',
+            '" . $etiqLaser->REVENDA ."'
             )";
 
         
@@ -37,18 +61,25 @@ curl_close($ch);
 
 //
 
-$numeroId = $_POST['etiqueta'];
-$copia = $_POST['copia'];
+$produto = $_GET['produto'];
+$produto = substr($produto, 3,6);
+$empresa = $_GET['empresa'];
 
-$buscaCarga .= " WHERE id_nota = '" . $numeroId . "' ";
+$buscaCarga .= " WHERE produto LIKE '%" . $produto . "%' ";
 $sucesso = $conn->query($buscaCarga);
 
 
 while($row = $sucesso->fetch_assoc()){
         echo "".$row['produto']."<br>
         &emsp;&emsp;NF ".$row['numero_nota']."<br>
-        &emsp;&emsp;".$row['caixa']."";
-    
+        &emsp;&emsp;".$row['caixa']."<br>";
+        $revenda = '1';
+        $endereco = "SELECT * FROM sisrev_etiqueta_estoque WHERE REVENDA = '".$revenda."'";
+        $deuCerto = $conn->query($endereco);
+
+        while($enderecoMostra = $deuCerto->fetch_assoc()){
+            echo '&emsp;'.$enderecoMostra['LOCACAO_ZONA'].'0'.$enderecoMostra['LOCACAO_RUA'].'&ensp;0'.$enderecoMostra['LOCACAO_ESTANTE'].'&ensp;'.$enderecoMostra['LOCACAO_PRATELEIRA'].'0'.$enderecoMostra['LOCACAO_NUMERO'].'';
+        }
     }
     
     $i++;
@@ -60,7 +91,7 @@ while($row = $sucesso->fetch_assoc()){
 
 
 
-<!-- <script>
+<script>
 window.onload = function () { window.print(); window.addEventListener("afterprint", function(event) { window.close(); });
     window.onafterprint(); } 
-</script> -->
+</script>
