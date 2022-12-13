a79fcc71b1170665f06c1b9a0914063abc1b8849
Felipe Silveira Lara <82624468+FelipeSilveiraL@users.noreply.github.com>
2022-12-13 15:53:47 -0300

    ajustes

--- sistemas/bpm/inc/inserindoTabelaAprovNF.php
+++ sistemas/bpm/inc/inserindoTabelaAprovNF.php
@@ -4,21 +4,9 @@ require_once('../config/query.php');
 
 $conexao = $conn->query($aprovNF);
 
-while($rowaprov = $conexao->fetch_assoc()){
+while ($rowaprov = $conexao->fetch_assoc()) {
 
-  $searchCompany = "SELECT NOME_EMPRESA FROM bpm_empresas WHERE ID_EMPRESA = ".$rowaprov['ID_EMPRESA']."";
-  $sucesso = $conn->query($searchCompany);
-  if($nameCompany = $sucesso->fetch_assoc()){
-    $name = $nameCompany['NOME_EMPRESA'];
-  }
-
-  $searchDep = "SELECT NOME_DEPARTAMENTO FROM bpm_rh_departamento WHERE ID_DEPARTAMENTO = ".$rowaprov['ID_DEPARTAMENTO']."";
-  $success = $conn->query($searchDep);
-  if($nameDep = $success->fetch_assoc()){
-    $nameDepartament = $nameDep['NOME_DEPARTAMENTO'];
-  }
-
-  switch($rowaprov['SITUACAO']){
+  switch ($rowaprov['SITUACAO']) {
     case 'A':
       $situacao = 'Ativo';
       break;
@@ -26,19 +14,22 @@ while($rowaprov = $conexao->fetch_assoc()){
       $situacao = 'Desativado';
       break;
   }
-echo '<tr>';
-echo '<td>' . $rowaprov['ID_APROVADOR'] . '</td>';
-echo '<td>' . $name . '';echo ($situacao == 'Ativo')? '<br><span class="badge rounded-pill bg-success">Ativo</span>' : '<br><span class="badge rounded-pill bg-secondary">Desativado</span>';echo'</td>';
-echo '<td>' . $nameDepartament . '</td>';
-echo '<td>' . $rowaprov['APROVADOR_FILIAL'] . '</td>';
-echo '<td>' . $rowaprov['APROVADOR_AREA'] . '</td>';
-echo '<td>' . $rowaprov['APROVADOR_MARCA'] . '</td>';
-echo '<td>' . $rowaprov['APROVADOR_GERENTE'] . '</td>';
-echo '<td>' . $rowaprov['APROVADOR_SUPERINTENDENTE'] . '</td>';
-// echo '<td>' . $situacao . '</td>';
-echo '<td ' . $usuarioFuncao . '><a href="editApNF.php?pg='.$_GET["pg"].'&id_aprovador='.$rowaprov["ID_APROVADOR"].'" title="Editar" class="btn-primary btn-sm" ><i class="bi bi-pencil"></i></a>
+
+  echo '<tr>';
+  echo '<td>' . $rowaprov['ID_APROVADOR'] . '</td>';
+  echo '<td>' . $rowaprov['NOME_EMPRESA'];
+  echo ($situacao == 'Ativo') ? '<br><span class="badge rounded-pill bg-success">Ativo</span>' : '<br><span class="badge rounded-pill bg-secondary">Desativado</span>';
+  echo '</td>';
+  echo '<td>' . $rowaprov['NOME_DEPARTAMENTO'] . '</td>';
+  echo '<td>' . $rowaprov['APROVADOR_FILIAL'] . '</td>';
+  echo '<td>' . $rowaprov['APROVADOR_AREA'] . '</td>';
+  echo '<td>' . $rowaprov['APROVADOR_MARCA'] . '</td>';
+  echo '<td>' . $rowaprov['APROVADOR_GERENTE'] . '</td>';
+  echo '<td>' . $rowaprov['APROVADOR_SUPERINTENDENTE'] . '</td>';
+  // echo '<td>' . $situacao . '</td>';
+  echo '<td ' . $usuarioFuncao . '><a href="editApNF.php?pg=' . $_GET["pg"] . '&id_aprovador=' . $rowaprov["ID_APROVADOR"] . '" title="Editar" class="btn-primary btn-sm" ><i class="bi bi-pencil"></i></a>
                             
-<a href= "http://'.$_SESSION['servidorOracle'].'/'.$_SESSION['smartshare'].'/bd/deletar.php?pg='.$_GET['pg'].'&id='.$rowaprov['ID_APROVADOR'] .'" title="Desativar" style="margin-top: 3px;" class="btn-danger btn-sm"><i class="bi bi-trash"></i></a>
+<a href= "http://' . $_SESSION['servidorOracle'] . '/' . $_SESSION['smartshare'] . '/bd/deletar.php?pg=' . $_GET['pg'] . '&id=' . $rowaprov['ID_APROVADOR'] . '" title="Desativar" style="margin-top: 3px;" class="btn-danger btn-sm"><i class="bi bi-trash"></i></a>
 </td> 
 </tr>
 ';
