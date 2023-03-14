8b808717210a3413e1bc733ba8713772dd48f5ce
Felipe Silveira Lara <82624468+FelipeSilveiraL@users.noreply.github.com>
2023-03-14 16:24:09 -0300

    Update contagemStatus.php

--- sistemas/notas/inc/contagemStatus.php
+++ sistemas/notas/inc/contagemStatus.php
@@ -3,7 +3,7 @@ require_once('../config/query.php');
 
 /*===================================*/
 
-$mesAnterior = '1'; //quantidade de meses anteriores
+$mesAnterior = '0'; //quantidade de meses anteriores
 $dataMes = " AND CL.date_create BETWEEN '".date('Y-m', strtotime('-'.$mesAnterior.' months', strtotime(date('Y-m-d'))))."-01' AND '".date('Y-m')."-31'";
 
 $queryCountLancando = "SELECT count(CL.id_lancarnotas) as countLancando FROM cad_lancarnotas CL WHERE CL.status_desc = 1 AND CL.ID_USUARIO = ".$_SESSION['id_usuario']." AND CL.deletar = 0 ".$dataMes;
