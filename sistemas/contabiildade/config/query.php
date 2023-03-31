7cb99460d5e2c4d5bfe7c0a5f07771ad2297a1e2
Felipe Silveira Lara <82624468+FelipeSilveiraL@users.noreply.github.com>
2023-03-31 15:53:53 -0300

    Update query.php

--- sistemas/contabiildade/config/query.php
+++ sistemas/contabiildade/config/query.php
@@ -9,4 +9,4 @@ CLU.numero_fluxo,
 U.nome
 FROM
 contabilidade_log_usuario CLU
-LEFT JOIN usuarios U ON (U.id_usuario = CLU.id_usuario)";
+LEFT JOIN usuarios U ON (U.id_usuario = CLU.id_usuario) ORDER BY  CLU.id DESC";
