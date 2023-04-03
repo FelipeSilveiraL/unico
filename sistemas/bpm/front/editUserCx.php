6d7e5381850dee0cb4da10fdd8e609c38240cf9e
Felipe Silveira Lara <82624468+FelipeSilveiraL@users.noreply.github.com>
2023-04-03 10:17:43 -0300

    update

--- sistemas/bpm/front/editUserCx.php
+++ sistemas/bpm/front/editUserCx.php
@@ -2,7 +2,6 @@
 require_once('head.php'); //CSS e configurações HTML e session start
 require_once('header.php'); //logo e login e banco de dados
 require_once('menu.php'); //menu lateral da pagina
-require_once('../../../config/databases.php');
 require_once('../../../config/sqlSmart.php');
 /* Essa opção descomentar após criar em telas_funcoes.php*/
 //echo $_GET['pg'] == '5' ?'': ' <script>window.location.href = "index.php";</script>';
@@ -36,116 +35,118 @@ require_once('../../../config/sqlSmart.php');
           <div class="card-body">
             <br>
             <?php
-            $id = $_GET['idCaixaEmpresa'];
-            $user = $_GET['user'];
+              $id = $_GET['idCaixaEmpresa'];
+              $user = $_GET['user'];
 
-            $consulta .= " WHERE ID_CAIXA_EMPRESA = " . $id . " AND USUARIO_CAIXA = '".$user."' ";
-            $result = oci_parse($connBpmgp, $consulta);
-            oci_execute($result);
-                  
-            while (($row = oci_fetch_assoc($result)) != false) {
+              $consulta .= " WHERE ID_CAIXA_EMPRESA = " . $id . " AND USUARIO_CAIXA = '" . $user . "' ";
+              /* $result = oci_parse($connBpmgp, $consulta);
+              oci_execute($result); */
 
-              $id_empresa = $row['ID_EMPRESA'];
+              echo $consulta;
+              exit;
 
-              //query que é só para exibir o nome do caixa referente ao dado cadastrado, por que só está dado numérico no db
+              while (($row = oci_fetch_assoc($result)) != false) {
 
-              $query = "SELECT NOME_CAIXA FROM caixa_empresa WHERE ID_CAIXA_EMPRESA = '".$row['ID_CAIXA_EMPRESA']."'";
-              
-              $result2 = oci_parse($connBpmgp, $query);
-              oci_execute($result2, OCI_COMMIT_ON_SUCCESS);
-                  
-            if (($row2 = oci_fetch_array($result2, OCI_ASSOC)) != false) {
-                $dado = $row2['NOME_CAIXA'];
+                $id_empresa = $row['ID_EMPRESA'];
+
+                //query que é só para exibir o nome do caixa referente ao dado cadastrado, por que só está dado numérico no db
+
+                $query = "SELECT NOME_CAIXA FROM caixa_empresa WHERE ID_CAIXA_EMPRESA = '" . $row['ID_CAIXA_EMPRESA'] . "'";
+
+                $result2 = oci_parse($connBpmgp, $query);
+                oci_execute($result2, OCI_COMMIT_ON_SUCCESS);
+
+                if (($row2 = oci_fetch_array($result2, OCI_ASSOC)) != false) {
+                  $dado = $row2['NOME_CAIXA'];
+                }
+
+                //query pra procurar se foi cadastrado alguem caixa nessa empresa, se não foi ele exibe um botão para cadastrar.
+                $queryNomeCaixa = "SELECT NOME_CAIXA,ID_CAIXA_EMPRESA FROM caixa_empresa WHERE ID_EMPRESA = '" . $id_empresa . "'";
+
+                $result3 = oci_parse($connBpmgp, $queryNomeCaixa);
+                oci_execute($result3, OCI_COMMIT_ON_SUCCESS);
+
+                if (($row3 = oci_fetch_array($result3, OCI_ASSOC)) != false) {
+                  $display = "none;";
+                } else {
+                  $display = "block;";
+                  $dado = 'Não existe caixa cadastrado!';
+                  $disabled = 'disabled';
+                }
+                //procura nome da empresa
+                $queryEmpresa .= " WHERE ID_EMPRESA =" . $id_empresa;
+                $result4 = oci_parse($connBpmgp, $queryEmpresa);
+                oci_execute($result4, OCI_COMMIT_ON_SUCCESS);
+
+                if (($row4 = oci_fetch_array($result4, OCI_ASSOC)) != false) {
+                  $nomeEmpresa = $row4['NOME_EMPRESA'];
+                }
+                // ---------------------------------
+                echo '
+                    <form method="POST" action=" ../inc/editarCxUs.php?pg=' . $_GET['pg'] . '&user=' . $user . '&id=' . $id . '" >
+                      <div class="row mb-3">
+                        <label for="user" class="col-sm-2 col-form-label">Nome Empresa</label>
+                        <div class="col-md-6">
+                          <input type="text" class="form-control" id="user" name="nome_empresa" value ="' . $nomeEmpresa . '" disabled>
+                          <input type="hidden" value="' . $row['USUARIO_CAIXA'] . '" name="usuario_caixa">
+                          <input type="hidden" value="' . $row['ID_EMPRESA'] . '" name="id_empresa" id="empresa">
+                        </div>
+                      </div>
+                      <div class="row mb-3">
+                        <label for="nomeCaixa" class="col-sm-2 col-form-label" required>Nome Caixa:</label>
+                        <div class="col-md-6">
+                          <select class="form-select" name="nomeCaixa" id="nomeCaixa" required ' . $disabled . '>
+                            <option value="' . $row['ID_CAIXA_EMPRESA'] . '" >' . $dado . '</option>
+                            <option>-------</option>';
+
+                //query que procura no bpm_caixa_empresa outros nomes cadastrados
+
+                $queryNomeCaixa = "SELECT NOME_CAIXA,ID_CAIXA_EMPRESA FROM caixa_empresa WHERE ID_EMPRESA = '" . $id_empresa . "'";
+
+                $result5 = oci_parse($connBpmgp, $queryNomeCaixa);
+                oci_execute($result5, OCI_COMMIT_ON_SUCCESS);
+
+                while (($nome = oci_fetch_array($result5, OCI_ASSOC)) != false) {
+
+                  echo '<option value="' . $nome['ID_CAIXA_EMPRESA'] . '">' . $nome['NOME_CAIXA'] . '</option>';
+                }
+
+                echo '</select>
+                        </div>
+                      </div>
+                      <div class="row mb-3">
+                        <label for="userCaixa" class="col-sm-2 col-form-label" required>Usuário Caixa:</label>
+                        <div class="col-md-6">
+                          <select class="form-select" name="userCaixa" id="userCaixa" required>
+                            <option value="' . $row['USUARIO_CAIXA'] . '">' . $row['USUARIO_CAIXA'] . '</option>
+                              <option value=""> ------------ </option>
+                              ';
+
+                $result6 = oci_parse($connSelbetti, $query_user);
+                oci_execute($result6, OCI_COMMIT_ON_SUCCESS);
+
+                while (($row6 = oci_fetch_array($result6, OCI_ASSOC)) != false) {
+
+                  echo '<option value="' . $row6['DS_LOGIN'] . '">' . $row6['DS_USUARIO'] . ' / ' . $row6['DS_LOGIN'] . '</option>';
+                }
+                echo '
+                          </select>
+                        </div>
+                      </div>
+                      
+                      <br>
+                      <div class="text-left">
+                        <button type="button" class="btn btn-primary"><a href="userCaixa.php?pg=' . $_GET['pg'] . '" style="color:white;">Voltar</a></button>
+                        <button type="submit" class="btn btn-success">Editar</button>
+                        <button type="button" class="btn btn-primary" style="display:' . $display . 'float:right;"><a href="novaRegraCxEmpresa.php?pg=' . $_GET['pg'] . '&id=' . $id_empresa . '" style="color:white;">Cadastrar</a></button>
+                      </div>
+                      
+                      <br>
+                    </form>';
               }
 
-              //query pra procurar se foi cadastrado alguem caixa nessa empresa, se não foi ele exibe um botão para cadastrar.
-              $queryNomeCaixa = "SELECT NOME_CAIXA,ID_CAIXA_EMPRESA FROM caixa_empresa WHERE ID_EMPRESA = '" . $id_empresa . "'";
-
-              $result3 = oci_parse($connBpmgp, $queryNomeCaixa);
-              oci_execute($result3, OCI_COMMIT_ON_SUCCESS);
-
-              if (($row3 = oci_fetch_array($result3, OCI_ASSOC)) != false) {
-                $display = "none;";
-              }else{
-                $display = "block;";
-                $dado = 'Não existe caixa cadastrado!';
-                $disabled = 'disabled';
-              }
-              //procura nome da empresa
-              $queryEmpresa .= " WHERE ID_EMPRESA =".$id_empresa;
-              $result4 = oci_parse($connBpmgp, $queryEmpresa);
-              oci_execute($result4, OCI_COMMIT_ON_SUCCESS);
-
-              if (($row4 = oci_fetch_array($result4, OCI_ASSOC)) != false) {
-                $nomeEmpresa = $row4['NOME_EMPRESA'];
-              }
-              // ---------------------------------
-              echo '
-                <form method="POST" action=" ../inc/editarCxUs.php?pg=' . $_GET['pg'] . '&user='.$user.'&id='.$id.'" >
-                  <div class="row mb-3">
-                    <label for="user" class="col-sm-2 col-form-label">Nome Empresa</label>
-                    <div class="col-md-6">
-                      <input type="text" class="form-control" id="user" name="nome_empresa" value ="' . $nomeEmpresa . '" disabled>
-                      <input type="hidden" value="' . $row['USUARIO_CAIXA'] . '" name="usuario_caixa">
-                      <input type="hidden" value="' . $row['ID_EMPRESA'] . '" name="id_empresa" id="empresa">
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <label for="nomeCaixa" class="col-sm-2 col-form-label" required>Nome Caixa:</label>
-                    <div class="col-md-6">
-                      <select class="form-select" name="nomeCaixa" id="nomeCaixa" required '.$disabled.'>
-                        <option value="'.$row['ID_CAIXA_EMPRESA'].'" >'.$dado.'</option>
-                        <option>-------</option>';
-                        
-                        //query que procura no bpm_caixa_empresa outros nomes cadastrados
-
-                        $queryNomeCaixa = "SELECT NOME_CAIXA,ID_CAIXA_EMPRESA FROM caixa_empresa WHERE ID_EMPRESA = '" . $id_empresa . "'";
-
-                        $result5 = oci_parse($connBpmgp, $queryNomeCaixa);
-                        oci_execute($result5, OCI_COMMIT_ON_SUCCESS);
-
-                        while (($nome = oci_fetch_array($result5, OCI_ASSOC)) != false) {
-
-                            echo '<option value="' . $nome['ID_CAIXA_EMPRESA'] . '">' . $nome['NOME_CAIXA'] . '</option>';
-
-                          }
-
-              echo '</select>
-                    </div>
-                  </div>
-                  <div class="row mb-3">
-                    <label for="userCaixa" class="col-sm-2 col-form-label" required>Usuário Caixa:</label>
-                    <div class="col-md-6">
-                      <select class="form-select" name="userCaixa" id="userCaixa" required>
-                        <option value="' . $row['USUARIO_CAIXA'] . '">' . $row['USUARIO_CAIXA'] . '</option>
-                          <option value=""> ------------ </option>
-                          '; 
-                          
-                          $result6 = oci_parse($connSelbetti, $query_user);
-                          oci_execute($result6, OCI_COMMIT_ON_SUCCESS);
-
-                          while (($row6 = oci_fetch_array($result6, OCI_ASSOC)) != false) {
-
-                            echo '<option value="' . $row6['DS_LOGIN'] . '">' . $row6['DS_USUARIO'] . ' / '.$row6['DS_LOGIN'].'</option>';
-                          }
-                          echo '
-                      </select>
-                    </div>
-                  </div>
-                  
-                  <br>
-                  <div class="text-left">
-                    <button type="button" class="btn btn-primary"><a href="userCaixa.php?pg=' . $_GET['pg'] . '" style="color:white;">Voltar</a></button>
-                    <button type="submit" class="btn btn-success">Editar</button>
-                    <button type="button" class="btn btn-primary" style="display:'.$display.'float:right;"><a href="novaRegraCxEmpresa.php?pg=' . $_GET['pg'] . '&id='.$id_empresa.'" style="color:white;">Cadastrar</a></button>
-                  </div>
-                  
-                  <br>
-                </form>';
-            }
-
-            oci_close($connBpmgp);
-            oci_close($connSelbetti);
+              oci_close($connBpmgp);
+              oci_close($connSelbetti);
             ?>
           </div>
         </div>
--- sistemas/bpm/front/userCaixa.php
+++ sistemas/bpm/front/userCaixa.php
@@ -3,7 +3,6 @@ require_once('head.php'); //CSS e configurações HTML e session start
 require_once('header.php'); //logo e login e banco de dados
 require_once('menu.php'); //menu lateral da pagina
 require_once('../config/query.php');
-require_once('../../../config/databases.php');
 require_once('../../../config/sqlSmart.php');
 /* Essa opção descomentar após criar em telas_funcoes.php*/
 //echo $_GET['pg'] == '5' ?'': ' <script>window.location.href = "index.php";</script>';
@@ -29,60 +28,60 @@ require_once('../../../config/sqlSmart.php');
   <!--################# COLE section AQUI #################-->
 
   <section class="section">
-      <div class="row">
-        <div class="col-lg-12">
+    <div class="row">
+      <div class="col-lg-12">
 
-          <div class="card">
-              <div class="card-header">
-                <a href="novaRegraCx.php?pg=<?= $_GET['pg'] ?>" type="button" class="btn btn-success buttonAdd" title="Importar Usuários" <?= $usuarioFuncao ?> ><i class="bi bi-plus-lg"></i></a>
-                <a href="../inc/relatorioUsuarioCaixa.php" type="button" class="btn btn-success" style="float: right;" title="Exportar excel"><i class="ri-file-excel-2-fill"></i></A>
-              </div><br>
-            <div class="card-body">
+        <div class="card">
+          <div class="card-header">
+            <a href="novaRegraCx.php?pg=<?= $_GET['pg'] ?>" type="button" class="btn btn-success buttonAdd" title="Importar Usuários" <?= $usuarioFuncao ?>><i class="bi bi-plus-lg"></i></a>
+            <a href="../inc/relatorioUsuarioCaixa.php" type="button" class="btn btn-success" style="float: right;" title="Exportar excel"><i class="ri-file-excel-2-fill"></i></A>
+          </div><br>
+          <div class="card-body">
 
-              <!-- Table with stripped rows -->
-              <table class="table table-striped datatable">
-                <thead>
-                  <tr>
-                    <th scope="col" class="capitalize">#</th>
-                    <th scope="col" class="capitalize">EMPRESA</th>
-                    <th scope="col" class="capitalize">NOME CAIXA</th>
-                    <th scope="col" class="capitalize">USUÁRIO</th>
-                    <th scope="col" class="capitalize" <?= $usuarioFuncao ?>>AÇÃO</th>
-                  </tr>
-                </thead>
-                <tbody>
-                 <?php 
-                  
-                  $result = oci_parse($connBpmgp, $query_caixa);
-                  oci_execute($result);
-                  
-                  while (($rowCaixa = oci_fetch_assoc($result)) != false) {
+            <!-- Table with stripped rows -->
+            <table class="table table-striped datatable">
+              <thead>
+                <tr>
+                  <th scope="col" class="capitalize">#</th>
+                  <th scope="col" class="capitalize">EMPRESA</th>
+                  <th scope="col" class="capitalize">NOME CAIXA</th>
+                  <th scope="col" class="capitalize">USUÁRIO</th>
+                  <th scope="col" class="capitalize" <?= $usuarioFuncao ?>>AÇÃO</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+
+                $result = oci_parse($connBpmgp, $query_caixa);
+                oci_execute($result);
+
+                while (($rowCaixa = oci_fetch_assoc($result)) != false) {
 
                   $idCaixaEmpresa = $rowCaixa['ID_CAIXA_EMPRESA'];
-                  echo'<tr>
-                  <td>'.$rowCaixa['id'].'</td>
-                  <td>'.$rowCaixa['NOME_EMPRESA'].'</td>
-                  <td>'.$rowCaixa['NOME_CAIXA'].'</td>
-                  <td>'.$rowCaixa["USUARIO_CAIXA"].'</td>
-                  <td><a href="editUserCx.php?pg=' . $_GET["pg"] . '&id='.$idCaixaEmpresa.'&user='.$rowCaixa["USUARIO_CAIXA"].'" title="Editar" class="btn-primary btn-sm" ' . $usuarioFuncao . '><i class="bi bi-pencil"></i></a>
-                            
-                  <a href="../inc/deletarCxUs.php?pg='.$_GET['pg'].'&id_empresa=' . $rowCaixa["ID_EMPRESA"] . '&usuario_caixa='.$rowCaixa["USUARIO_CAIXA"].'&id_caixa_empresa='.$idCaixaEmpresa.'" title="Desativar" class="btn-danger btn-sm" ' . $usuarioFuncao . '><i class="bi bi-trash"></i></a></td>
-                  </tr>';
-                 }
-                 
-                 
-                 oci_close($connBpmgp);
-                 ?>
-                </tbody>
-              </table>
-              <!-- End Table with stripped rows -->
+                  echo '<tr>
+                          <td>' . $rowCaixa['id'] . '</td>
+                          <td>' . $rowCaixa['NOME_EMPRESA'] . '</td>
+                          <td>' . $rowCaixa['NOME_CAIXA'] . '</td>
+                          <td>' . $rowCaixa["USUARIO_CAIXA"] . '</td>
+                          <td><a href="editUserCx.php?pg=' . $_GET["pg"] . '&id=' . $idCaixaEmpresa . '&user=' . $rowCaixa["USUARIO_CAIXA"] . '" title="Editar" class="btn-primary btn-sm" ' . $usuarioFuncao . '><i class="bi bi-pencil"></i></a>
+                                    
+                          <a href="../inc/deletarCxUs.php?pg=' . $_GET['pg'] . '&id_empresa=' . $rowCaixa["ID_EMPRESA"] . '&usuario_caixa=' . $rowCaixa["USUARIO_CAIXA"] . '&id_caixa_empresa=' . $idCaixaEmpresa . '" title="Desativar" class="btn-danger btn-sm" ' . $usuarioFuncao . '><i class="bi bi-trash"></i></a></td>
+                        </tr>';
+                }
 
-            </div>
+                oci_free_statement($result);
+                oci_close($connBpmgp);
+                ?>
+              </tbody>
+            </table>
+            <!-- End Table with stripped rows -->
+
           </div>
-
         </div>
+
       </div>
-    </section>
+    </div>
+  </section>
 
   <!--################# section TERMINA AQUI #################-->
 
